e1971820a2c6db363dd6b5eb58b171f8fcad978f
dr0nx <kovbozh@gmail.com>
2021-01-27 17:47:02 +0300

    Выполнил задачу task5 урока №3

--- lesson3/task4.py
+++ lesson3/task4.py
@@ -14,12 +14,12 @@ def my_func2(x, y):
 
 
 try:
-    erection = float(input("Введите действительное положительное число x: "))
+    raised = float(input("Введите действительное положительное число x: "))
     power = int(input("Введите целое отрицательное число y: "))
     if power >= 0:
         print("Необходимо ввести целое отрицательное число!")
 
-    print("Первый метод: возведение в степень с помощью оператора **: ", my_func1(erection, power))
-    print("Второй метод: использование цикла: ", my_func2(erection, power))
+    print("Первый метод: возведение в степень с помощью оператора **: ", my_func1(raised, power))
+    print("Второй метод: использование цикла: ", my_func2(raised, power))
 except ValueError:
     print("Необходимо ввести число!")
