f7423e4c65fabb5a9a37f096075590e88027a6f2
dr0nx <kovbozh@gmail.com>
2021-02-01 10:12:00 +0300

    Выполнил задание task3.py.txt урока №5

---
+++ lesson5/task3.py
@@ -0,0 +1,17 @@
+"""
+Создать текстовый файл (не программно), построчно записать фамилии сотрудников
+и величину их окладов. Определить, кто из сотрудников имеет оклад менее 20 тыс.,
+вывести фамилии этих сотрудников. Выполнить подсчет средней величины дохода сотрудников.
+"""
+
+with open("text3.txt", "r") as f_obj:
+    text = f_obj.read().splitlines()
+    average = 0
+    for index, el in enumerate(text):
+        split_line = text[index].split()
+        salary = int(split_line[2])
+        if salary < 20000:
+            first_last = split_line[0] + ' ' + split_line[1]
+            print(f'{first_last} получает менее 20000 т. р. ({salary})')
+        average += salary
+    print(f'Средняя величина дохода сотрудников: {average / index + 1:.2f} т.р.')
